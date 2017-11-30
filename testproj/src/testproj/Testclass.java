6fe77b7fc5a055ae3e9b977da21c8ca6c1905e3c
gitmail007 <gitmail007@gmail.com>
2017-11-30 16:51:31 -0700

    Added comments

--- testproj/src/testproj/Testclass.java
+++ testproj/src/testproj/Testclass.java
@@ -5,6 +5,7 @@ public class Testclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a test code");
+		System.out.println("This is an update");
 	}
 
 }
